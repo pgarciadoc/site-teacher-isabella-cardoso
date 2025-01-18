b4176c8794f2ce67a4ce939c73aba35117e7d8ba
Pedro Garcia <pgpessoal@outlook.com>
2025-01-17 21:57:26 -0300

    site-v3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-// const hamburger = document.querySelector(".hamburger");
-// const nav = document.querySelector(".nav");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
 
-// hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+hamburger.addEventListener("click", () => nav.classList.toggle("active"));
